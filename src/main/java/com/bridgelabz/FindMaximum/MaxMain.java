da20320463a47ecba7f4aff400a2a7b4de5c8a5d
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-08 13:01:22 +0530

    [Prerna]REFACTORED: Created a generic class with 3 generic type variables

--- src/main/java/com/bridgelabz/FindMaximum/MaxMain.java
+++ src/main/java/com/bridgelabz/FindMaximum/MaxMain.java
@@ -1,15 +1,35 @@
 package com.bridgelabz.FindMaximum;
 
-public class MaxMain {
+public class MaxMain<E extends Comparable<E>> {
+	E xobj;
+	E yobj;
+	E zobj;
+
+	// Parameterized constructor
+	public MaxMain(E xobj, E yobj, E zobj) {
+		super();
+		this.xobj = xobj;
+		this.yobj = yobj;
+		this.zobj = zobj;
+	}
+
+	public MaxMain() {
+		super();
+	}
+
 	// Find maximum between 3 Objects
-	public <E extends Comparable<E>> E findMaximum(E x, E y, E z) {
-		E max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
+	public static <E extends Comparable<E>> E findMaximum(E a, E b, E c) {
+		E max = a;
+		if (b.compareTo(max) > 0) {
+			max = b;
 		}
-		if (z.compareTo(max) > 0) {
-			max = z;
+		if (c.compareTo(max) > 0) {
+			max = c;
 		}
 		return max;
+	}
+
+	public E findMaximum() {
+		return MaxMain.findMaximum(xobj, yobj, zobj);
 	}
 }
--- src/test/java/com/bridgelabz/FindMaximum/MaxTest.java
+++ src/test/java/com/bridgelabz/FindMaximum/MaxTest.java
@@ -2,68 +2,79 @@ package com.bridgelabz.FindMaximum;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class MaxTest {
-	MaxMain m = null;
-
-	@Before
-	public void initialize() {
-		m = new MaxMain();
-	}
 
 	@Test
 	public void givenThreeIntegers_whenMaxAtPosition1_ReturnPosition1Integer() {
-		Integer max = m.findMaximum(10, 9, 3);
+		Integer max = MaxMain.findMaximum(10, 9, 3);
 		assertEquals((Integer) 10, max);
 	}
 
 	@Test
 	public void givenThreeIntegers_whenMaxAtPosition2_ReturnPosition2Integer() {
-		Integer max = m.findMaximum(6, 9, 3);
+		Integer max = MaxMain.findMaximum(6, 9, 3);
 		assertEquals((Integer) 9, max);
 	}
 
 	@Test
 	public void givenThreeIntegers_whenMaxAtPosition3_ReturnPosition3Integer() {
-		Integer max = m.findMaximum(1, 2, 3);
+		Integer max = MaxMain.findMaximum(1, 2, 3);
 		assertEquals((Integer) 3, max);
 	}
 
 	@Test
 	public void givenThreeFloats_whenMaxAtPosition1_ReturnPosition1Float() {
-		Double max = m.findMaximum(10.2, 9.4, 3.7);
+		Double max = MaxMain.findMaximum(10.2, 9.4, 3.7);
 		assertEquals((Double) 10.2, max);
 	}
 
 	@Test
 	public void givenThreeFloats_whenMaxAtPosition2_ReturnPosition2Float() {
-		Double max = m.findMaximum(6.2, 9.3, 3.8);
+		Double max = MaxMain.findMaximum(6.2, 9.3, 3.8);
 		assertEquals((Double) 9.3, max);
 	}
 
 	@Test
 	public void givenThreeFloats_whenMaxAtPosition3_ReturnPosition3Float() {
-		Double max = m.findMaximum(1.1, 2.2, 3.3);
+		Double max = MaxMain.findMaximum(1.1, 2.2, 3.3);
 		assertEquals((Double) 3.3, max);
 	}
 
 	@Test
 	public void givenThreeStrings_whenMaxAtPosition1_ReturnPosition1String() {
-		String max = m.findMaximum("Peach", "Apple", "Banana");
+		String max = MaxMain.findMaximum("Peach", "Apple", "Banana");
 		assertEquals("Peach", max);
 	}
 
 	@Test
 	public void givenThreeStrings_whenMaxAtPosition2_ReturnPosition2String() {
-		String max = m.findMaximum("Apple", "Peach", "Banana");
+		String max = MaxMain.findMaximum("Apple", "Peach", "Banana");
 		assertEquals("Peach", max);
 	}
 
 	@Test
 	public void givenThreeStrings_whenMaxAtPosition3_ReturnPosition3String() {
-		String max = m.findMaximum("Apple", "Banana", "Peach");
+		String max = MaxMain.findMaximum("Apple", "Banana", "Peach");
+		assertEquals("Peach", max);
+	}
+
+	@Test
+	public void givenGenericClassInteger_ReturnMaxInteger() {
+		Integer max = new MaxMain<Integer>(4, 3, 2).findMaximum();
+		assertEquals((Integer) 4, max);
+	}
+
+	@Test
+	public void givenGenericClassFloat_ReturnMaxFloat() {
+		Double max = new MaxMain<Double>(4.4, 3.3, 2.2).findMaximum();
+		assertEquals((Double) 4.4, max);
+	}
+
+	@Test
+	public void givenGenericClassString_ReturnMaxString() {
+		String max = new MaxMain<String>("Apple", "Banana", "Peach").findMaximum();
 		assertEquals("Peach", max);
 	}
 }
