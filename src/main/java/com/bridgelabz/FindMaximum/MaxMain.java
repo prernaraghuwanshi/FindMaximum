261b6df3116b3cb3addf049fb98c1b2a3af4749f
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-08 12:29:51 +0530

    [Prerna]REFACTORED: Combined all 3 functions to 1 generic method

--- src/main/java/com/bridgelabz/FindMaximum/MaxMain.java
+++ src/main/java/com/bridgelabz/FindMaximum/MaxMain.java
@@ -1,33 +1,9 @@
 package com.bridgelabz.FindMaximum;
 
 public class MaxMain {
-	// Find maximum between 3 integers
-	public Integer findMaximum(Integer x, Integer y, Integer z) {
-		Integer max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
-		return max;
-	}
-
-	// Find maximum between 3 floats
-	public Double findMaximum(Double x, Double y, Double z) {
-		Double max = x;
-		if (y.compareTo(max) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
-		return max;
-	}
-
-	// Find maximum between 3 strings
-	public String findMaximum(String x, String y, String z) {
-		String max = x;
+	// Find maximum between 3 Objects
+	public <E extends Comparable<E>> E findMaximum(E x, E y, E z) {
+		E max = x;
 		if (y.compareTo(max) > 0) {
 			max = y;
 		}
