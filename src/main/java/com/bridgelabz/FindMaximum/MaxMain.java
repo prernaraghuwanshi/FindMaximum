c8c3bdbb3b88c62440f62feefbc084d5e80b8a80
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-08 13:40:41 +0530

    [Prerna]ADDED: Print method for 3 elements and for array

--- src/main/java/com/bridgelabz/FindMaximum/MaxMain.java
+++ src/main/java/com/bridgelabz/FindMaximum/MaxMain.java
@@ -15,10 +15,6 @@ public class MaxMain<E extends Comparable<E>> {
 		this.zobj = zobj;
 	}
 
-	public MaxMain() {
-		super();
-	}
-
 	// Find maximum between 3 Objects
 	public static <E extends Comparable<E>> E findMaximum(E a, E b, E c) {
 		E max = a;
@@ -28,16 +24,33 @@ public class MaxMain<E extends Comparable<E>> {
 		if (c.compareTo(max) > 0) {
 			max = c;
 		}
+		printMax(a, b, c, max);
 		return max;
-	}
-
-	// Find maximum for more than 3 Objects using Options and Sorting
-	public static <E extends Comparable<E>> E findMaximum(E... arr) {
-		Arrays.sort(arr);
-		return arr[arr.length - 1];
 	}
 
 	public E findMaximum() {
 		return MaxMain.findMaximum(xobj, yobj, zobj);
 	}
+
+	// Print function for 3 elements
+	public static <E> void printMax(E a, E b, E c, E max) {
+		System.out.println("Max of " + a + ", " + b + ", " + c + " is " + max);
+	}
+
+	// Find maximum for more than 3 Objects using Options and Sorting
+	public static <E extends Comparable<E>> E findMaximum(E... arr) {
+		Arrays.sort(arr);
+		E max = arr[arr.length - 1];
+		printMax(max, arr);
+		return max;
+	}
+
+	// Print function for more than 3 objects
+	public static <E> void printMax(E max, E... arr) {
+		System.out.println("Array is ");
+		for (E iterator : arr)
+			System.out.print(" " + iterator + " ");
+		System.out.println("\nMaximum is " + max);
+	}
+
 }
