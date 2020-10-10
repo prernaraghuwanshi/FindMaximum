d685878cc5662dcd845cd16205e361c87d24f41d
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-10 08:38:46 +0530

    [Prerna]REFACTORED: One function to find maximum for 3 elements or more

--- src/main/java/com/bridgelabz/FindMaximum/MaxMain.java
+++ src/main/java/com/bridgelabz/FindMaximum/MaxMain.java
@@ -3,56 +3,55 @@ package com.bridgelabz.FindMaximum;
 import java.util.Arrays;
 
 public class MaxMain<E extends Comparable<E>> {
-	E xobj;
-	E yobj;
-	E zobj;
+	E firstGenericObject;
+	E secondGenericObject;
+	E thirdGenericObject;
 
-	// Parameterized constructor
-	public MaxMain(E xobj, E yobj, E zobj) {
-		super();
-		this.xobj = xobj;
-		this.yobj = yobj;
-		this.zobj = zobj;
+	public MaxMain(E firstGenericObject, E secondGenericObject, E thirdGenericObject) {
+		this.firstGenericObject = firstGenericObject;
+		this.secondGenericObject = secondGenericObject;
+		this.thirdGenericObject = thirdGenericObject;
 	}
 
-	// Find maximum between 3 Objects
-	public static <E extends Comparable<E>> E findMaximum(E a, E b, E c) {
-		E max = a;
-		if (b.compareTo(max) > 0) {
-			max = b;
+	// Find maximum for 3 elements or more
+	public static <E extends Comparable<E>> E findMaximum(E firstGeneric, E secondGeneric, E thirdGeneric,
+			E... genericArray) {
+		E maxGeneric = firstGeneric;
+		if (secondGeneric.compareTo(maxGeneric) > 0) {
+			maxGeneric = secondGeneric;
 		}
-		if (c.compareTo(max) > 0) {
-			max = c;
+		if (thirdGeneric.compareTo(maxGeneric) > 0) {
+			maxGeneric = thirdGeneric;
 		}
-		printMax(a, b, c, max);
-		return max;
+		if (genericArray.length > 0)
+			maxGeneric = findMaxGenericArray(maxGeneric, genericArray);
+		printMax(firstGeneric, secondGeneric, thirdGeneric, maxGeneric, genericArray);
+		return maxGeneric;
+	}
+
+	// Find maximum for more than 3 elements
+	private static <E extends Comparable<E>> E findMaxGenericArray(E maxGeneric, E[] genericArray) {
+		for (E genericElement : genericArray) {
+			if (genericElement.compareTo(maxGeneric) > 0)
+				maxGeneric = genericElement;
+		}
+		return maxGeneric;
 	}
 
 	public E findMaximum() {
-		return MaxMain.findMaximum(xobj, yobj, zobj);
+		return MaxMain.findMaximum(firstGenericObject, secondGenericObject, thirdGenericObject);
 	}
 
-	// Print function for 3 elements
-	public static <E> void printMax(E a, E b, E c, E max) {
-		System.out.println("Max of " + a + ", " + b + ", " + c + " is " + max);
-	}
-
-	// Find maximum for more than 3 Objects using Options and Sorting
-	@SafeVarargs
-	public static <E extends Comparable<E>> E findMaximum(E... arr) {
-		Arrays.sort(arr);
-		E max = arr[arr.length - 1];
-		printMax(max, arr);
-		return max;
-	}
-
-	// Print function for more than 3 objects
-	@SafeVarargs
-	public static <E> void printMax(E max, E... arr) {
-		System.out.println("Array is ");
-		for (E iterator : arr)
-			System.out.print(" " + iterator + " ");
-		System.out.println("\nMaximum is " + max);
+	// Print function
+	public static <E> void printMax(E firstGeneric, E secondGeneric, E thirdGeneric, E maxGeneric,
+			E... genericArray) {
+		System.out.println("Given Array:- ");
+		System.out.println(firstGeneric + "\n" + secondGeneric + "\n" + thirdGeneric);
+		if (genericArray.length > 0) {
+			for (E iterator : genericArray)
+				System.out.print(iterator + "\n");
+		}
+		System.out.println("\nMaximum element is:- " + maxGeneric);
 	}
 
 }
